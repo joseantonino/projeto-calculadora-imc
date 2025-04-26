fa13360bf53c881c082bbad72ca3fff68fe1f340
joseantonino <joseantonino78@gmail.com>
2025-04-26 16:49:38 -0300

    atualização projeto calculadora IMC

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const calcular = document.getElementById("calcular");
 
 function imc() {
-  const nome = document.getElementById("nome");
-  const altura = document.getElementById("altura");
-  const peso = document.getElementById("peso");
+  const nome = document.getElementById("nome").value;
+  const altura = document.getElementById("altura").value;
+  const peso = document.getElementById("peso").value;
   const resultado = document.getElementById("resultado");
 
-  if (nome !== "") {
-    alert("nome vazio");
+  if (nome != "" && altura != "" && peso != "") {
+    const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+    let classificacao = "";
+
+    if (valorIMC < 18.5) {
+      classificacao = "abaixo do peso.";
+    } else if (valorIMC < 25) {
+      classificacao = "com peso ideal. Parabéns!!!.";
+    } else if (valorIMC < 30) {
+      classificacao = "levimente acima do peso.";
+    } else if (valorIMC < 35) {
+      classificacao = "com obesidade grau I.";
+    } else if (valorIMC < 40) {
+      classificacao = "com obesidade grau II";
+    } else {
+      classificacao = "com odesidade grau III. Cuidado!!.";
+    }
+
+    resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+  } else {
+    resultado.textContent = "preencha todos os campos!!!.";
   }
 }
 
